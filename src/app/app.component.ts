da2db599bdc3f67bcb730b3c7eb9610f10e554c4
adilelmoba <adilelmoba20@gmail.com>
2024-12-13 23:23:46 +0100

    add/ fontawesome, layout components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from "./layout/navigation/navigation.component";
+import { LibraryComponent } from "./layout/library/library.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FontAwesomeModule,
+    NavigationComponent,
+    LibraryComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'spotify-clone-front';
+export class AppComponent implements OnInit {
+  
+  private faIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
